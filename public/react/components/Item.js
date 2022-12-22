165c14b7e647d043c882440efd25b0f15ad42473
botirmasharipov <masbotirjon963@gmail.com>
2022-12-21 20:35:52 -0500

    feat: create Item component

---
+++ public/react/components/Item.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react"
+import apiURL from "../api"
+
+export const Page = () => {
+    const [singlePageView, setSinglePageView] = useState(false)
+
+    const fetchItem = async () => {
+        try {
+            // single article page
+            const res = await fetch(`${apiURL}/item/`)
+            const data = await res.json()
+            console.log(data)
+            // TODO: handle state
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchItem()
+    }, [])
+
+    const showSinglePageView = () => {
+        fetchItem()
+        setSinglePageView(!singlePageView)
+    }
+
+    const handleBackButton = async () => {
+        setSinglePageView(!singlePageView)
+    }
+
+    return (
+        <>
+            {singlePageView ? (
+                <>
+                    <h3>{props.item.name}</h3>
+                    <img src={props.itemimage} alt={props.item.name} />
+                    <button onClick={handleBackButton}>Back to Wiki List</button>
+                </>
+
+            ) : (<h3 onClick={showSinglePageView}>Item</h3>)
+            }
+        </>
+    )
+}
