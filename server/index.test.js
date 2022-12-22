2c970a4aae40f204a6dc74222ed495ff0d484cd9
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2022-12-22 15:46:14 -0500

    test: adding CRUD tests for Item || feat: repopulating database after running test by adding seed.js to test script responsibilities in .json

--- server/index.test.js
+++ server/index.test.js
@@ -16,4 +16,25 @@ describe('Sauce, Item Models', () => {
         expect(testItem.category).toEqual("men's clothing")
         expect(testItem.image).toEqual("https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg")
     })
+
+    test('Can find Item', async () => {
+        const testItem = await Item.findByPk(1)
+        expect(testItem.title).toEqual("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
+    })
+
+    test('Can update Item', async () => {
+        const testItem = await Item.findByPk(1)
+        const updatedItem = await testItem.update({
+            title: "update"
+        })
+        expect(updatedItem.title).toEqual("update")
+        expect(updatedItem.price).toEqual(109.95)
+    })
+
+    test('Can delete Item', async () => {
+        const testItem = await Item.findByPk(1)
+        await testItem.destroy()
+        const foundItems = await Item.findAll()
+        expect(foundItems.length).toEqual(0)
+    })
 })
