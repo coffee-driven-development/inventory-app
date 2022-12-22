cf29b6b75950df50302bba1dc99d202939b0f64b
botirmasharipov <masbotirjon963@gmail.com>
2022-12-22 12:47:11 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/coffee-driven-development/inventory-app""
    This reverts commit d9a6c3cb7d87a15c5a67487d2563203559ec7075.

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,3 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { Item } = require('./Item')
 
-const Sauce = sequelize.define("sauces", {
-  name: Sequelize.STRING,
-  image: Sequelize.STRING,
-});
-
-module.exports = {
-  db: sequelize,
-  Sauce,
-};
+module.exports = { Item }
