279876061f2b77e8c55ba8835168f329b1dff595
pierrepnguyen <pierrepnguyen@gmail.com>
2023-01-04 08:38:23 -0800

    delete bug fix

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -64,6 +64,7 @@ export const App = () => {
       <Item
         setSelectedPage={setSelectedPage}
         currentItem={currentItem}
+        fetchItems={fetchItems}
       />
     </main>
   )}
--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -4,16 +4,36 @@ import apiURL from "../api";
 export const Item = ({
   setSelectedPage,
   currentItem,
+  fetchItems
 }) => {
 
   const currentItemID = currentItem.id
 
-  const handleDelete=async(currentItemID)=>{
+  const fetchDelete=async(currentItemID)=>{
     const res = await fetch(`${apiURL}/items/${currentItemID}`, {
       method: "DELETE"
     })
+    const data = await res.json()
+    fetchItems()
     setSelectedPage("Main View")
   }
+
+  const handleDelete = (e) => {
+    fetchDelete(e.target.value)
+  }
+
+  // const handleDelete=async(currentItemID)=>{
+  //   try {
+  //     const requestOptions = {
+  //         method: 'DELETE',
+  //         headers: {'Content-Type': 'application/JSON'}
+  //     }
+  //     const response = await fetch(`${apiURL}/items/${currentItemID}`, requestOptions)
+  //     setSelectedPage("Main View")
+  //   } catch (err) {
+  //     console.log("Oh no an error! ", err);
+  //   }
+  // }
 
   const handleBackButton = async () => {
     setSelectedPage("Main View");
