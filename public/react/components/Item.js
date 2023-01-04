197aef45e8d0984bd228c150b079c375a2b6711c
pierrepnguyen <pierrepnguyen@gmail.com>
2023-01-03 21:09:02 -0800

    single page css

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -13,11 +13,15 @@ export const Item = ({
   return (
       <>
           <h3 className="single-item-header">{currentItem.title}</h3>
-          <img src={currentItem.image} alt={currentItem.name} className="image single-item" />
-          <p className="single-item">{currentItem.description}</p>
-          <p className="single-item">{currentItem.price}</p>
-          <p className="single-item">{currentItem.category}</p>
-          <button onClick={handleBackButton} className='button-59'>Back to Items List</button>
+          <div className="item-content">
+            <img src={currentItem.image} alt={currentItem.name} className="image" />
+            <p className="price">${currentItem.price}</p>
+            <p className="single-item"><span className="bold">Description: </span>{currentItem.description}</p>
+            <p className="single-item"><span className="bold">Category: </span>{currentItem.category}</p>
+          </div>
+          <div className="buttons-div">
+            <button onClick={handleBackButton} className='button-59'>Back to Items List</button>
+          </div>
       </>
   );
 };
